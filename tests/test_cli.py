ed9cb0426f6e37bb2fa99b41c1b72bcf163b83a8
Philipp Kats <casyfill@gmail.com>
2023-06-13 22:00:30 -0400

    stdout

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -66,6 +66,7 @@ def test_cli_update():
         ],
     )
 
-    assert result.exit_code == 0
+    assert result.exit_code == 0, result.stdout
+
     string_to_be = f'Writing with `{CURRENT_PROTOCOL_VERSION}` to `{output_path}`"'
     assert string_to_be in result.stderr
